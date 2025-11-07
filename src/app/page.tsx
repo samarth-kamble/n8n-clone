4380408babaaa0b42f773cfd5546e621e9252f67
Samarth Kamble <kamblesamarth04@gmail.com>
2025-11-07 23:21:04 +0530

    04-Authentication : Implement the trpc for auth.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
   await requireAuth();
 
+  const data = await caller.getUsers();
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       Protected Server Components
+      {JSON.stringify(data)}
     </div>
   );
 };
