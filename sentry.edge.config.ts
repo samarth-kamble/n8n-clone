26f39ffacd94451f825a62f8436f0737111c9e3e
Samarth Kamble <kamblesamarth04@gmail.com>
2025-11-11 00:16:22 +0530

    08-Error-Tracking : Implement the sentry error tracking.

---
+++ sentry.edge.config.ts
@@ -0,0 +1,24 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cf1c29490c3696531ea15f885c6b3b23@o4507029866086400.ingest.us.sentry.io/4510342156976128",
+  integrations: [
+    Sentry.consoleLoggingIntegration({
+      levels: ["log", "error", "warn"], // Capture console.error and console.warn
+    }),
+  ],
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
